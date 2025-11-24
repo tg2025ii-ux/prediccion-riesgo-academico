bc046f56e6cb6d9289c9f655e9d466152e3587ea
tg2025ii-ux <tg2025ii@gmail.com>
2025-11-24 07:02:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -369,7 +369,7 @@ elif menu == "📤 Cargar Datos":
                             # ============================================================
                             
                             st.markdown(f"""
-                            <div style='padding: 1rem; background-color: {COLORS['danger']}; border-radius: 10px; border-left: 4px solid {COLORS['danger']};'>
+                            <div style='padding: 1rem; background-color: {COLORS['background']}; border-radius: 10px; border-left: 4px solid {COLORS['background']};'>
                                 <h4 style='color: {COLORS['danger']}; margin: 0;'>⏳ Procesando datos (Limpieza → Encoding → Ajustes)...</h4>
                             </div>
                             """, unsafe_allow_html=True)
